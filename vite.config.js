3ff9f98de72daac3ea591066d5e074386ce55c29
yasvanthika122 <118598932+yasvanthika122@users.noreply.github.com>
2023-11-01 22:25:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://mern-rent-client.onrender.com',
         secure: false,
       },
     },
